cda3e8b5051dbde7ade80b86e7aafd4b1eda46bd
babic <Barbara.Cintoli@fao.org>
2017-03-20 15:06:40 +0100

    Domain Page

--- src/nls/en/labels.js
+++ src/nls/en/labels.js
@@ -1,4 +1,8 @@
 /*global define*/
 define({
 
+    "download_data" : "DOWNLOAD DATA",
+    "visualize_data" : "VISUALIZE DATA",
+    "20": "Indicator 20: Number of accessions conserved ex situ under medium or long-term conditions"
+
 });
