e371202015ed914f9f51e9a6b1ea1abe554feeb9
babic <Barbara.Cintoli@fao.org>
2017-03-22 15:56:48 +0100

    WIEWS Domains Structure Creation

--- src/nls/en/labels.js
+++ src/nls/en/labels.js
@@ -3,6 +3,9 @@ define({
 
     "download_data" : "DOWNLOAD DATA",
     "visualize_data" : "VISUALIZE DATA",
-    "20": "Indicator 20: Number of accessions conserved ex situ under medium or long-term conditions"
+    "20": "Indicator 20: Number of accessions conserved ex situ under medium or long-term conditions",
+
+    "loading_in_progress" : "Loading in progress",
+    "completed" : "Completed"
 
 });
