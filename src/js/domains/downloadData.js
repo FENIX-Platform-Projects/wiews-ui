28135ea1f4da94cad19e4fc261695e2708745d5d
Triani, Mario <Mario.Triani@fao.org>
2017-03-31 17:08:51 +0200

    updates

--- src/js/domains/downloadData.js
+++ src/js/domains/downloadData.js
@@ -78,10 +78,13 @@ define([
         this.lang = Clang;
         this.environment = C.ENVIRONMENT;
         this.cache = C.cache;
-        this.indicatorConfig = INDICATORSC[this.indicator];
+        this.indicatorConfig = INDICATORSC[this.indicator.position];
         this.config = this.indicatorConfig[dashboardName];
         this.channels = {};
-        this.dashboardConfig = "";
+        this.dashboardConfig = this.config['dashboard'];
+        //this.dashboardConfig.environment = this.environment;
+
+        console.log(this.dashboardConfig);
 
         this._bindEventListeners();
 
@@ -91,8 +94,9 @@ define([
 
         if(this.dashboard) this._disposeDashboard();
 
-        var dashboardConf = this._getElemConfig(s.dashboard.dashboard_config_item),
-            filterConfig = this._getElemConfig(s.filter.filter_config_item);
+        var filterConfig = this._getElemConfig(s.filter.filter_config_item);
+
+        console.log(filterConfig);
 
         this._renderFilter(filterConfig);
 
@@ -103,8 +107,10 @@ define([
 
         var self = this;
 
+
+
         $(s.button.show_data).on('click', function () {
-            self._renderDashboard();
+            self._renderDashboard(self.filter.getValues());
         });
 
     };
@@ -117,8 +123,7 @@ define([
 
         var config;
 
-        if((this.config)&&(elem)&&(this.config[elem]))
-            config = this.config[elem];
+        if((this.config)&&(elem)&&(this.config[elem])) config = this.config[elem];
 
         return config;
     }
@@ -137,12 +142,163 @@ define([
         });
     };
 
-    DownloadData.prototype._renderDashboard = function () {
+    DownloadData.prototype._renderDashboard = function (filters) {
         // Build new dashboard
 
+        console.log(this.dashboardConfig);
+
+        this.dashboardConfig.filter = this.filter.getValues();
+
+        var adamconf = {
+            "uid": "adam_cpf_undaf_priorities_table",
+            "items": [
+                {
+                    "id": "dd_dashboard_item",
+                    "type": "table",
+                    "config": {
+                        "groupedRow": false,
+                        "formatter": "localstring",
+                        "showRowHeaders": true,
+                        "rows": [
+                            "recipientcode",
+                            "purposecode",
+                            "undaf_stated_priority",
+                            "cpf_stated_priority"
+                        ],
+                        "aggregations": [],
+                        "inputFormat": "fenixtool",
+                        "config": {
+                            "pageSize": 150,
+                            "autoSelectFirstRow": false,
+                            "columns": [
+                                {
+                                    "id": "recipientcode",
+                                    "fieldIndex": "0",
+                                    "width": 150
+                                },
+                                {
+                                    "id": "purposecode",
+                                    "fieldIndex": "1",
+                                    "width": 200
+                                },
+                                {
+                                    "id": "undaf_stated_priority",
+                                    "fieldIndex": "2",
+                                    "width": 220
+                                },
+                                {
+                                    "id": "cpf_stated_priority",
+                                    "fieldIndex": "3",
+                                    "width": 220
+                                }
+                            ]
+                        }
+                    },
+                    "filterFor": {
+                        "filter_priorities": [
+                            "recipientcode"
+                        ]
+                    },
+                    "postProcess": [
+                        {
+                            "name": "filter",
+                            "sid": [
+                                {
+                                    "uid": "adam_cpf_undaf_priorities_table"
+                                }
+                            ],
+                            "parameters": {
+                                "columns": [
+                                    "purposecode",
+                                    "undaf_stated_priority",
+                                    "cpf_stated_priority",
+                                    "undaf_period",
+                                    "cpf_period",
+                                    "recipientcode"
+                                ],
+                                "rows": {
+                                    "recipientcode": {
+                                        "codes": [
+                                            {
+                                                "uid": "crs_recipients",
+                                                "version": "2016",
+                                                "codes": [
+                                                    "625"
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+                            "rid": {
+                                "uid": "filter_priorities"
+                            }
+                        },
+                        {
+                            "name": "group",
+                            "parameters": {
+                                "by": [
+                                    "purposecode",
+                                    "undaf_stated_priority",
+                                    "cpf_stated_priority",
+                                    "undaf_period",
+                                    "cpf_period",
+                                    "recipientcode"
+                                ],
+                                "aggregations": []
+                            }
+                        }
+                    ]
+                }
+            ],
+/*
+            "filter": {
+                "valid": true,
+                "labels": {
+                    "recipientcode": {
+                        "555": "Afghanistan"
+                    },
+                    "donorcode": {
+                        "all": "All"
+                    },
+                    "year-from": [],
+                    "year-to": [],
+                    "year": {
+                        "range": "2015-2015"
+                    }
+                },
+                "values": {
+                    "recipientcode": [
+                        "555"
+                    ],
+                    "donorcode": [],
+                    "year-from": [],
+                    "year-to": [],
+                    "year": [
+                        {
+                            "value": "2015",
+                            "parent": "from"
+                        },
+                        {
+                            "value": "2015",
+                            "parent": "to"
+                        }
+                    ],
+                    "uid": []
+                }
+            },
+*/
+            "environment": "demo"
+        };
+
+        adamconf.filter = this.filter.getValues();
+
         this.dashboard = new Dashboard(
-            dashboardConfig
+          //  this.dashboardConfig
+            adamconf
         );
+
+        console.log(this.dashboardConfig);
 
         // Build fake data
         /*
