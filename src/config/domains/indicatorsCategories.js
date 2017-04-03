890a1e50e6369bc9f9c588ee68d64d32ec203e7e
Mario Triani <mario.triani@fao.org>
2017-04-03 22:08:57 +0200

    updates on selectors

--- src/config/domains/indicatorsCategories.js
+++ src/config/domains/indicatorsCategories.js
@@ -7,9 +7,9 @@ define(function () {
             position : "1",
             processType : "1",
             downloadConversion : {
-                dd_filter_item_1_1 : "m49_country",
-                dd_filter_item_1_2 : "",
-                dd_filter_item_1_3 : "",
+                dd_filter_item_1_1 : "iso3",
+                dd_filter_item_1_2 : "m49_country",
+                dd_filter_item_1_3 : "fao_regions",
                 dd_filter_item_2 : "element",
                 dd_filter_item_3 : "iteration",
                 dd_filter_item_4 : "stackholder"
