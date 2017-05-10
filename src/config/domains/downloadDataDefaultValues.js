4c270e68cae8d82cbd213e9063b9168201063fe4
babic <Barbara.Cintoli@fao.org>
2017-05-10 14:50:36 +0200

    Indicator 2 Default Values

--- src/config/domains/downloadDataDefaultValues.js
+++ src/config/domains/downloadDataDefaultValues.js
@@ -20,6 +20,22 @@ define(function () {
         "1_filter-dd_filter_item_9" : ["1"],
         "1_filter-dd_filter_item_10" : ["stk"],
         "1_filter-dd_filter_item_11" : ["1"],
-        "1_filter-dd_filter_item_12" : ["1", "2", "3"]
+        "1_filter-dd_filter_item_12" : ["1", "2", "3"],
+        //2= indicator id
+        "2_filter-dd_filter_item_1" :  ["ABW","AFG","AGO","AIA"],
+        "2_filter-dd_filter_item_2" : ["1"],
+        "2_filter-dd_filter_item_3" : ["1", "2"],
+        "2_filter-dd_filter_item_4_1" : ["5000"],
+        "2_filter-dd_filter_item_4_2" : ["21"],
+        "2_filter-dd_filter_item_4_3" : ["744"],
+        "2_filter-dd_filter_item_4_4" : ["826"],
+        "2_filter-dd_filter_item_5" : ["1"],
+        "2_filter-dd_filter_item_6" : ["1", "2"],
+        "2_filter-dd_filter_item_7_1" : ["007"],
+        "2_filter-dd_filter_item_7_2" : ["36"],
+        "2_filter-dd_filter_item_8" : ["20"],
+        "2_filter-dd_filter_item_9" : ["1"],
+        "2_filter-dd_filter_item_10" : ["1"],
+        "2_filter-dd_filter_item_11" : ["1", "2", "3"]
     };
 });
--- src/config/domains/visualizeDataDefaultValues.js
+++ src/config/domains/visualizeDataDefaultValues.js
@@ -17,6 +17,20 @@ define(function () {
         "1_filter-vd_filter_item_7_1" : ["007"],
         "1_filter-vd_filter_item_7_2" : ["36"],
         "1_filter-vd_filter_item_8" : ["abarema"],
-        "1_filter-vd_filter_item_9" : ["1"]
+        "1_filter-vd_filter_item_9" : ["1"],
+
+        //2= indicator id
+        "2_filter-vd_filter_item_1" :  ["ABW","AFG","AGO","AIA","ALA","ALB","AND","ARE","ARG","ARM","ASM","ATA","ATF","ATG","AUS","AUT","AZE","BDI","BEL","BEN","BES","BFA","BGD","BGR","BHR","BHS","BIH","BLM","BLR","BLZ","BMU","BOL","BRA","BRB","BRN","BTN","BVT","BWA","CAF","CAN","CCK","CHE","CHL","CHN","CIV","CMR","COD","COG","COK","COL","COM","CPV","CRI","CUB","CUW","CXR","CYM","CYP","CZE","DEU","DJI","DMA","DNK","DOM","DZA","ECU","EGY","ERI","ESH","ESP","EST","ETH","FIN","FJI","FLK","FRA","FRO","FSM","GAB","GBR","GEO","GGY","GHA","GIB","GIN","GLP","GMB","GNB","GNQ","GRC","GRD","GRL","GTM","GUF","GUM","GUY","HKG","HMD","HND","HRV","HTI","HUN","IDN","IMN","IND","IOT","IRL","IRN","IRQ","ISL","ISR","ITA","JAM","JEY","JOR","JPN","KAZ","KEN","KGZ","KHM","KIR","KNA","KOR","KWT","LAO","LBN","LBR","LBY","LCA","LIE","LKA","LSO","LTU","LUX","LVA","MAC","MAF","MAR","MCO","MDA","MDG","MDV","MEX","MHL","MKD","MLI","MLT","MMR","MNE","MNG","MNP","MOZ","MRT","MSR","MTQ","MUS","MWI","MYS","MYT","NAM","NCL","NER","NFK","NGA","NIC","NIU","NLD","NOR","NPL","NRU","NZL","OMN","PAK","PAN","PCN","PER","PHL","PLW","PNG","POL","PRI","PRK","PRT","PRY","PSE","PYF","QAT","REU","ROU","RUS","RWA","SAU","SDN","SEN","SGP","SGS","SHN","SJM","SLB","SLE","SLV","SMR","SOM","SPM","SRB","SSD","STP","SUR","SVK","SVN","SWE","SWZ","SXM","SYC","SYR","TCA","TCD","TGO","THA","TJK","TKL","TKM","TLS","TON","TTO","TUN","TUR","TUV","TWN","TZA","UGA","UKR","UMI","URY","USA","UZB","VAT","VCT","VEN","VGB","VIR","VNM","VUT","WLF","WSM","YEM","ZAF","ZMB","ZWE"],
+        "2_filter-vd_filter_item_2" : ["1"],
+        "2_filter-vd_filter_item_3" : ["1", "2"],
+        "2_filter-vd_filter_item_4_1" : ["5000"],
+        "2_filter-vd_filter_item_4_2" : ["21"],
+        "2_filter-vd_filter_item_4_3" : ["744"],
+        "2_filter-vd_filter_item_4_4" : ["826"],
+        "2_filter-vd_filter_item_5" : ["1"],
+        "2_filter-vd_filter_item_6" : ["1", "2"],
+        "2_filter-vd_filter_item_7_1" : ["007"],
+        "2_filter-vd_filter_item_7_2" : ["36"],
+        "2_filter-vd_filter_item_8" : ["1"]
     };
 });
