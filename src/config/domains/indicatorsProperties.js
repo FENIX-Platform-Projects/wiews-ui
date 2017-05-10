f5e09c1c371bc282ac009533947f5d8fd87ed325
babic <Barbara.Cintoli@fao.org>
2017-05-10 14:36:50 +0200

    Indicator 2 properties

--- src/config/domains/indicatorsProperties.js
+++ src/config/domains/indicatorsProperties.js
@@ -10,6 +10,14 @@ define(function () {
             dd_dashboard_category : "1",
             vd_dashboard_category : "1",
             processType : "1"
+        },
+        "2": {
+            indicator_id : "2",
+            dd_filter_category : "2",
+            vd_filter_category : "2",
+            dd_dashboard_category : "1",
+            vd_dashboard_category : "2",
+            processType : "2"
         }
     }
 });
